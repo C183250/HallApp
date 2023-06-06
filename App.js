21496addfa6aafea67a3e22e6224b84eaae00558
Rajia Sultana <101783174+C183250@users.noreply.github.com>
2023-06-06 20:34:00 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {component} from 'react';
+import {Button, View, Text} from 'react-native';
+
+export default class App extends component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View>
+        <Text> Hall App</Text>
+      </View>
+    );
+  }
+}
